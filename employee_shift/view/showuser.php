10634ea4aa3dc6b444803c3ae09d56d505c19bc4
tasik50 <158137475+tasik50@users.noreply.github.com>
2025-02-02 22:28:46 +0600

    Update showuser.php

--- employee_shift/view/showuser.php
+++ employee_shift/view/showuser.php
@@ -1,25 +1,56 @@
-
+<?php
+include '../control/showuser_control.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <link rel="stylesheet" href="../css/showuser.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration Information</title>
+    <title>Show User Payroll</title>
+    
 </head>
 <body>
-    <h1>Registration Information</h1>
+    <header class="page-heading">
+        <h1>User Payroll Information</h1>
+    </header>
+
     <a href="../control/session_logout.php">Logout</a>
     <a href="../view/edituser.php">Update</a>
     <a href="../view/deleteuser.php">Delete</a>
-    
+    <a href="../view/searchuser.php">Search</a>
 
-    <br><br>
-
-    
+    <table border="1">
+        <thead>
+            <tr>
+            <th>id</th>
+                    <th>userid</th>
+                    <th>fromdate</th>
+                    <th>todate</th>
+                    <th>start_time</th>
+                    <th>end_time</th>
+                    <th>shift_time</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            if (!empty($shiftdata)) {
+                foreach ($shiftdata as $row) {
+                    echo "<tr>
+                        <td>{$row['id']}</td>
+                        <td>{$row['userid']}</td>
+                        <td>{$row['fromdate']}</td>
+                        <td>{$row['todate']}</td>
+                        <td>{$row['start_time']}</td>
+                        <td>{$row['end_time']}</td>
+                        <td>{$row['shift_time']}</td>
+                    </tr>";
+                }
+            } else {
+                echo "<tr><td colspan='7'>No payroll records found.</td></tr>";
+            }
+            ?>
+        </tbody>
+    </table>
 </body>
 </html>
-<?php
-//session_start(); // Start the session
-include '../control/showuser_control.php'; // Include control logic
-?>
